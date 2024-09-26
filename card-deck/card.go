d90b8a41bd537f847f4739ba99717fb8f1ec9cd5
jaz <lordxarus@gmail.com>
2024-09-26 18:54:37 -0400

    card deck is done

---
+++ card-deck/card.go
@@ -0,0 +1,170 @@
+//go:generate stringer -type=Suit,Rank
+package deck
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Heart
+	Club
+	Joker // this is a special case
+)
+
+var suits = [...]Suit{Spade, Diamond, Heart, Club}
+
+type Rank uint8
+
+const (
+	Ace Rank = iota + 1
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+const (
+	minRank = Ace
+	maxRank = King
+)
+
+type Card struct {
+	Suit
+	Rank
+}
+
+func (c Card) String() string {
+	if c.Suit == Joker {
+		return c.Suit.String()
+	}
+	return fmt.Sprintf("%s of %ss", c.Rank, c.Suit)
+}
+
+// functional options, or as I prefer to
+// call them. Functional operators
+
+func New(opts ...func([]Card) []Card) []Card {
+	var cards []Card
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+
+	for _, opt := range opts {
+		cards = opt(cards)
+	}
+	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	// being able to modify parameters is
+	// whacky!!
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+// Create a sorter based with lessFn logic
+func Sort(lessFn func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		// We capture lessFn with the closure
+		// This part looks identical to DefaultSort
+		// we just parameterized the lessFn
+		sort.Slice(cards, lessFn(cards))
+		return cards
+	}
+}
+
+// i and j are indices into cards
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+// this is basically like converting from 2d coords (rank, suit) to 1d coords (absRank)
+// (x, y) -> i
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
+
+// We're not shuffling inline, like Sort(), for simplicity's sake
+func Shuffle(cards []Card) []Card {
+	ret := make([]Card, len(cards))
+	r := rand.New(rand.NewSource(uint64(time.Now().Unix())))
+	// Perm(5)
+	// [0, 4, 2, 1, 3]
+
+	// We use these elements to index into ret slice.
+	// And set ret[i] to the card value at the
+	// idx j of the cards slice
+	perm := r.Perm(len(cards))
+	for i, j := range perm {
+		ret[i] = cards[j]
+	}
+
+	return ret
+}
+
+//
+/*
+
+If we just had:
+
+func Jokers(cards []Card) []Card
+
+we would have to use it like New(Joker, Joker, Joker)
+to create three jokers. Instead we just return a function that
+loops over n
+
+
+*/
+func Jokers(n int) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{
+				Rank: Rank(i),
+				Suit: Joker,
+			})
+		}
+		return cards
+	}
+}
+
+func Filter(f func(card Card) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for _, c := range cards {
+			if !f(c) {
+				ret = append(ret, c)
+			}
+		}
+		return ret
+	}
+}
+
+func Deck(n int) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for i := 0; i < n; i++ {
+			ret = append(ret, cards...)
+		}
+		return ret
+	}
+}
---
+++ card-deck/card_test.go
@@ -0,0 +1,81 @@
+package deck
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleCard() {
+	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Two, Suit: Spade})
+	fmt.Println(Card{Rank: Nine, Suit: Diamond})
+	fmt.Println(Card{Rank: Jack, Suit: Club})
+	fmt.Println(Card{Suit: Joker})
+
+	// Output:
+	// Ace of Hearts
+	// Two of Spades
+	// Nine of Diamonds
+	// Jack of Clubs
+	// Joker
+
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	// 13 ranks * 4 suits
+	if len(cards) != 13*4 {
+		t.Errorf("wrong num of cards in a new deck: %d", len(cards))
+	}
+}
+
+// I don't think these sorting tests actually test anything
+// New() returns a sorted deck by default
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	// without these parenthesis we fail to compile
+	if cards[0] != (Card{Rank: Ace, Suit: Spade}) {
+		t.Error("wanted Ace of Spades as first card, but got", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	if cards[0] != (Card{Rank: Ace, Suit: Spade}) {
+		t.Error("wanted Ace of Spades as first card, but got", cards[0])
+	}
+}
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	if count != 3 {
+		t.Error("expected 3 jokers, got", count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func(c Card) bool {
+		return c.Rank == Two || c.Rank == Three
+	}
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if filter(c) {
+			t.Errorf("found: %s, but it was meant to be filtered", c)
+		}
+	}
+}
+
+func TestDeck(t *testing.T) {
+	cards := New(Deck(3))
+	// 13 ranks * 4 suits * 3 decks
+	if len(cards) != 13*4*3 {
+		t.Errorf("expected %d cards, got %d instead", 13*4*3, len(cards))
+	}
+}
